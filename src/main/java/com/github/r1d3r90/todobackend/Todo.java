f5eed39c899ef45aaa8a1e76467e1c4e4355c4e9
R1d3r90 <hubertlion@aol.com>
2024-06-11 18:17:00 +0200

    github auto deployment

--- src/main/java/com/github/r1d3r90/todobackend/Todo.java
+++ src/main/java/com/github/r1d3r90/todobackend/Todo.java
@@ -1,8 +1,5 @@
 package com.github.r1d3r90.todobackend;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
 public record Todo(String id, String description, String status) {
 
 }
